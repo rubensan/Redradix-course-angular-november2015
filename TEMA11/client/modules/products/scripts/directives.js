5063af8aca2f82e6ed89e755a2f0ca77d857f6c8
daniboomerang <estevez.dani@gmail.com>
2015-10-23 17:35:41 +0200

    Web Project: Displaying product info with directives: descriptive-element and technical-specifications. Improving products.json. Styling new directives and improving the previous ones.

--- TEMA11/client/modules/products/scripts/directives.js
+++ TEMA11/client/modules/products/scripts/directives.js
@@ -32,3 +32,70 @@ productsDirectives.directive('product', function($state, Products) {
     }
   };
 });
+
+productsDirectives.directive('descriptiveElement', function($compile) {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '='  // Using the same name as the attribute
+    },
+    compile: function compile(tElement, tAttrs, transclude) {
+    
+      function createHtmlSubelementText(text, classes) {
+        var html = '<div class="descriptive-subelement-wrapper ' + classes + '"> <div class="descriptive-subelement">' + text + '</div></div>';
+        return html;
+      }
+
+      function createHtmlSubelementImage(src, classes) {
+        var html = '<div class="descriptive-subelement-wrapper ' + classes + '"><div class="descriptive-subelement"><img class="img-responsive" alt="" src="' + src + '"></div></div>';
+        return html;     
+      }
+
+      function createHtml(descriptiveElement) {
+        var html;
+        if (descriptiveElement.type == 'text-image') {
+          html = '<div class="descriptive-element" style="background:' + descriptiveElement.background + '">' + createHtmlSubelementText(descriptiveElement.description, 'left') + createHtmlSubelementImage(descriptiveElement.image, 'right') + '</div>';
+          return html;
+            
+        }
+        else if (descriptiveElement.type == 'image-text') {
+          html = '<div class="descriptive-element" style="background:' + descriptiveElement.background + '">' + createHtmlSubelementImage(descriptiveElement.image, 'left') +createHtmlSubelementText(descriptiveElement.description, 'right') +  '</div>';
+          return html;
+            
+        }
+        else if (descriptiveElement.type == 'text') {
+          html = '<div class="descriptive-element" style="background:' + descriptiveElement.background + '">' + createHtmlSubelementText(descriptiveElement.description, 'center') + '</div>';
+          return html;
+            
+        }
+        else { 
+          html = '<div class="descriptive-element" style="background:' + descriptiveElement.background + '">' + createHtmlSubelementImage(descriptiveElement.image, 'center') + '</div>';
+          return html;
+        } 
+      }
+
+      return {
+        pre: function preLink(scope, iElement, iAttrs) { 
+          var html = createHtml(scope.info)
+          iElement.append(html);
+        },
+        post: function postLink(scope, iElement, iAttrs) { }
+      }
+    }
+  };
+});
+
+productsDirectives.directive('technicalSpecifications', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '='  // Using the same name as the attribute
+    },
+    templateUrl: 'modules/products/views/technical-specifications.html',
+    controller: function($scope) {
+      $scope.$watch('info', function() { 
+        if ($scope.info != undefined) { $scope.design = $scope.info.design; } 
+      })
+    }  
+  };
+});
