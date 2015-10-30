fbf738be2762bb44a0e9b74e8873986334cd2e8e
daniboomerang <estevez.dani@gmail.com>
2015-10-30 21:40:02 +0100

    Updating Exercises and Slides

--- TEMA2/EJERCICIOS/Solucion Dani/gna.js
+++ TEMA2/EJERCICIOS/Solucion Dani/gna.js
@@ -1,14 +1,15 @@
 /* Our App is an Angular Module */
-var gnaUIlogic = angular.module('gnaUIlogic', []);        
+var gnaUIlogic = angular.module('gnaUIlogic', []);
 
-/* Creating a new controler for aur app */  
-gnaUIlogic.controller('gnaUIlogicCtrl', function ($scope, $interval){ 
-    function generateRandomNumber (){return Math.floor((Math.random() * 10) + 1)}
-    var stop;
-    $scope.randomNumber;
-    $scope.generateRandomNumber = function() {
-        $interval(function() {
+/* Creating a new controler for aur app */  	
+gnaUIlogic.controller('gnaUIlogicCtrl', function ($scope, $interval){		
+	function generateRandomNumber (){return Math.floor((Math.random() * 10) + 1)}
+    $scope.randomNumber = '-';
+    var intervalPromise;
+    $scope.generateRandomNumber = function() {  
+        intervalPromise = $interval(function() {
             $scope.randomNumber = generateRandomNumber();
         }, 1000);
-    };      
+    };
+    $scope.$on('$destroy', function () { $interval.cancel(intervalPromise); });
 });
